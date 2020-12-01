b43dd5a177893d6f1a58e847acd92be55449d436
MrOsteoporosis <sanderverheijen@outlook.com>
2020-12-01 16:34:48 +0100

    now using strncmp to check for null termination in strdup tests

--- strduptest.c
+++ strduptest.c
@@ -6,7 +6,7 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/18 13:12:50 by averheij      #+#    #+#                 */
-/*   Updated: 2020/11/10 15:01:18 by averheij      ########   odam.nl         */
+/*   Updated: 2020/12/01 16:29:42 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ int		strdup_test(char *src, size_t size)
 	ft = _ft_strdup(src);
 	syserrno = errno;
 
-	cmp = strcmp(sys, ft);
+	cmp = strncmp(sys, ft, strlen(src + 1));
 	if (cmp)
 		fail = 0;
 	if (fterrno != syserrno)
@@ -61,3 +61,5 @@ int		strdup_test(char *src, size_t size)
 		error_out("Failed strdup test");
 	return (fail);
 }
+
+
